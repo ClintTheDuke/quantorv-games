9a7d28209ffc8266dddeae50330d826a71b6222a
ClintTheDuke <clintontheduke@gmail.com>
2026-06-18 19:03:17 +0100

    update

--- main.js
+++ main.js
@@ -45,52 +45,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-    // ===== Substack Newsletter Code ========
+
  
 
-document
-  .getElementById("newsletterForm")
-  .addEventListener("submit", async function (e) {
-    e.preventDefault();
 
-    const emailInput = document.getElementById("emailInput");
-    const message = document.getElementById("newsletterMessage");
-
-    const email = emailInput.value.trim();
-    if (!email) return;
-
-    message.style.display = "block";
-    message.textContent = "Processing...";
-
-    try {
-      await subscribeUser(email);
-
-      message.textContent =
-        "Almost done! Check your inbox to confirm your subscription.";
-
-      emailInput.value = "";
-    } catch (err) {
-      console.error(err);
-      message.textContent =
-        "Something went wrong. Please try again later.";
-    }
-  });
-
-
-// ===== PROVIDER FUNCTION (THIS IS THE ONLY PART YOU EVER EDIT) =====
-
-async function subscribeUser(email) {
-
-  // CURRENT METHOD: Substack redirect
-  const substackUrl =
-    "https://quantorvgames.substack.com/subscribe?email=" +
-    encodeURIComponent(email) +
-    "&utm_source=website&utm_medium=newsletter_form";
-
-  window.open(substackUrl, "_blank");
-
-  return true;
-}
  /* ======== QVG Carousel  ========= */
     async function qvgLoad3DCarousel() {
        
